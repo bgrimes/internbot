cda90990330165ab30cb6c92810364724dd327a2
Anthony Marion <asuahsahua@gmail.com>
2013-11-22 12:21:47 -0500

    Readyify for heroku deploy

---
+++ add-heroku-vars.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+heroku config:add HUBOT_HIPCHAT_JID="hipchat jabber id"
+heroku config:add HUBOT_HIPCHAT_PASSWORD="password"
+heroku config:add HUBOT_HIPCHAT_JOIN_ROOMS_ON_INVITE="chatroom"
+heroku config:add HUBOT_LOG_LEVEL="debug"
